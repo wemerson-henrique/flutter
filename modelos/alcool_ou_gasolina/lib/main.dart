cdb492950f06d6b7dfe6c6076d70508e674038d3
Wemerson Henrique <68334448+wemerson-henrique@users.noreply.github.com>
2020-08-21 17:14:29 -0300

    terminei álcool ou gasolina

--- modelos/alcool_ou_gasolina/lib/main.dart
+++ modelos/alcool_ou_gasolina/lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:alcoolougasolina/src/home/home.dart';
 import 'package:alcoolougasolina/src/my_widget/input_widget.dart';
+import 'package:alcoolougasolina/src/my_widget/loading_button_widget.dart';
 import 'package:alcoolougasolina/src/my_widget/logo.dart';
+import 'package:alcoolougasolina/src/my_widget/submit_form_widget.dart';
+import 'package:alcoolougasolina/src/my_widget/success_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
@@ -21,93 +25,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-
-  var controle_gasolina = new MoneyMaskedTextController();//cria um comtrolador
-  var controle_alcool = new MoneyMaskedTextController();//cria um comtrolador
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,//recebe a cor primaria da aplicatico "cor do plano de fundo"
-      body: ListView(
-        children: <Widget>[
-          Logo(),
-          Container(
-            margin: EdgeInsets.all(30),//margem do conteiner
-            decoration: BoxDecoration(//quando se usa BoxDecoration nao pode colocar a cor fora
-              color: Colors.white.withOpacity(0.8),
-               borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  "Compesa utilisar Álcool",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 40,
-                    fontFamily: "Big Shoulders Display"
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  margin: EdgeInsets.all(30),//margem do conteiner
-                  height: 60,
-                  width: double.infinity,
-                  decoration: BoxDecoration(//quando se usa BoxDecoration nao pode colocar a cor fora
-                    color: Colors.white.withOpacity(0.8),
-                    borderRadius: BorderRadius.circular(60),
-                  ),
-                  child: FlatButton(
-                    child: Text(
-                      "CALCULAR NOVAMENTE",
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 25,
-                        fontFamily: "Big Shoulders Display",
-                      ),
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Input(
-              label: "Gasolina",
-              controle: controle_gasolina,
-          ),
-          Input(
-            label: "Álcool",
-            controle: controle_alcool,
-          ),
-          Container(
-            margin: EdgeInsets.all(30),//margem do conteiner
-            height: 60,
-            decoration: BoxDecoration(//quando se usa BoxDecoration nao pode colocar a cor fora
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(60),
-            ),
-            child: FlatButton(
-              child: Text(
-                  "CALCULAR",
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 25,
-                  fontFamily: "Big Shoulders Display",
-                ),
-              ),
-              onPressed: () {},
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
--- modelos/alcool_ou_gasolina/lib/src/my_widget/loading_button_widget.dart
+++ modelos/alcool_ou_gasolina/lib/src/my_widget/loading_button_widget.dart
@@ -1,6 +1,52 @@
-class Button extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+class LoadingButton extends StatelessWidget {
+
+  var busy = false;
+  var invert = false;
+  Function funcao;
+  var text = "";
+
+  LoadingButton({
+    @required this.busy,
+    @required this.invert,
+    @required this.funcao,
+    @required this.text,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return
+      busy ? Container(
+        alignment: Alignment.center,
+        height: 50,
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.white,
+        ),
+      )
+      :
+      Container(
+      margin: EdgeInsets.all(30),//margem do conteiner
+      height: 60,
+      width: double.infinity,
+      decoration: BoxDecoration(//quando se usa BoxDecoration nao pode colocar a cor fora
+        color: invert
+            ? Theme.of(context).primaryColor
+            : Colors.white.withOpacity(0.8),
+        borderRadius: BorderRadius.circular(60),
+      ),
+      child: FlatButton(
+        child: Text(
+          text,
+          style: TextStyle(
+            color:
+            invert ? Colors.white : Theme.of(context).primaryColor,
+            fontSize: 25,
+            fontFamily: "Big Shoulders Display",
+          ),
+        ),
+        onPressed: funcao,
+      ),
+    );
   }
 }
